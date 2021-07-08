10b00e8677f1b3146caf050fd04f99b7b1e6c70c
Mian Jawad Ahmad <a.jawad321@gmail.com>
2021-07-08 15:00:22 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,34 +18,37 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("First App"),
         ),
-        body: Center(
-            child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(colors: [
-                Colors.pink,
-                Colors.yellow,
-              ]),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 10,
-                )
-              ]),
-          child: Text(
-            "I am the box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+        body: Container(
+          color: Colors.teal,
+          width: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            width: 100,
+            height: 100,
+            color: Colors.red,
+            alignment: Alignment.center,
           ),
-        )));
+          Container(
+            padding: const EdgeInsets.all(8),
+            width: 100,
+            height: 100,
+            color: Colors.yellow,
+            alignment: Alignment.center,
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            width: 100,
+            height: 100,
+            color: Colors.green,
+            alignment: Alignment.center,
+          )
+            ],
+          ),
+        ),
+      );
   }
 }
