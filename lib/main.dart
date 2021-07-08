f4dcbcba9367da482cfa45b8f6459a5c8aeb6059
Mian Jawad Ahmad <a.jawad321@gmail.com>
2021-07-08 14:35:23 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,51 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   // widgets app //materialApp // CupertinoApp
   runApp(MaterialApp(
     home: HomePage(),
     theme: ThemeData(
       primarySwatch: Colors.purple,
-      ),
+    ),
   ));
 }
 
-class  HomePage extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
-        title: Text('First App'),
+        appBar: AppBar(
+          title: Text("First App"),
         ),
-        body: Container(
-          child: Text('Hi Flutter'),
-        )
-    );
+        body: Center(
+            child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [
+                Colors.pink,
+                Colors.yellow,
+              ]),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 10,
+                )
+              ]),
+          child: Text(
+            "I am the box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )));
   }
 }
