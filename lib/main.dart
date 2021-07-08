8e2909eddcc666eb9e2712e7011e19fb2125276c
Mian Jawad Ahmad <a.jawad321@gmail.com>
2021-07-08 15:31:00 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,36 +18,50 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("First App"),
         ),
-        body: Container(
-          color: Colors.teal,
-          width: 500,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+        body: Center(
+          child: Container(
+            color: Colors.teal,
+            height: 100,
+            width: 100,
+           
+          ),
+        ),drawer: Drawer(
+          child: ListView(
+            padding: const EdgeInsets.all(0),
             children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            color: Colors.red,
-            alignment: Alignment.center,
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            color: Colors.yellow,
-            alignment: Alignment.center,
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            color: Colors.green,
-            alignment: Alignment.center,
-          )
+              UserAccountsDrawerHeader(accountName: 
+              Text('Mian Jawad Ahmad'), accountEmail: 
+              Text('a.jawad321@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage('https://miro.medium.com/fit/c/344/344/1*fRAdaj62YuIAqF9Q7LBxvA@2x.jpeg')
+              )
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Mian Jawad Ahmad"),
+                subtitle: Text("Developer"),
+                trailing: Icon(Icons.edit),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.email),
+                title: Text("Email"),
+                subtitle: Text("mianjawadahmad@encoderbytes.com"),
+                trailing: Icon(Icons.edit),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Mian Jawad Ahmad"),
+                subtitle: Text("Flutter learning"),
+                trailing: Icon(Icons.edit),
+              ),
             ],
           ),
+          ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.edit),
         ),
       );
   }
